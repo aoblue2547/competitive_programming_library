80c96e1734f44c126be22f088f81c964921464e8
kodama <79963508+aoblue2547@users.noreply.github.com>
2025-12-23 17:54:34 +0900

    update Treap.cpp

--- Treap.cpp
+++ Treap.cpp
@@ -1,24 +1,11 @@
 template<class S, S(*op)(S, S), S(*e)()>
 class Treap {
-	class xorshift {
-		ull x;
-	public:
-		xorshift() {
-			mt19937 rnd(chrono::steady_clock::now().time_since_epoch().count());
-			x = rnd();
-			for (int i = 0; i < 100; i++) {
-				random();
-			}
-		}
+	mt19937 mt;
+	uniform_int_distribution<int> rnd;
 
-		ull random() {
-			x = x ^ (x << 7);
-			return x = x ^ (x >> 9);
-		}
-	} rnd;
 	struct Node {
 		S val;
-		Node* lch, *rch;
+		Node* lch, * rch;
 		int pri;
 		int siz;
 		S sum;
@@ -28,7 +15,7 @@ class Treap {
 			rch = nullptr;
 		}
 	};
-	Node* root;
+	Node* root = nullptr;
 
 	int size(Node* t) {
 		if (t == nullptr)return 0;
@@ -51,7 +38,7 @@ class Treap {
 
 	Node* update(Node* t) {
 		t->siz = size(t->lch) + size(t->rch) + 1;
-		t->sum = op(op(prod(t->lch), prod(t->rch)), t->val);
+		t->sum = op(op(prod(t->lch), t->val), prod(t->rch));
 		return t;
 	}
 
@@ -62,12 +49,12 @@ class Treap {
 		if (r == nullptr)return l;
 
 		if (l->pri > r->pri) {
-			l->rch = merge(move(l->rch), move(r));
-			return update(move(l));
+			l->rch = merge(l->rch, r);
+			return update(l);
 		}
 		else {
-			r->lch = merge(move(l), move(r->lch));
-			return update(move(r));
+			r->lch = merge(l, r->lch);
+			return update(r);
 		}
 	}
 
@@ -76,37 +63,37 @@ class Treap {
 
 		push(t);
 		if (k <= size(t->lch)) {
-			pair<Node*, Node*> s = split(move(t->lch), k);
-			t->lch = move(s.second);
-			return { move(s.first),update(move(t)) };
+			pair<Node*, Node*> s = split(t->lch, k);
+			t->lch = s.second;
+			return { s.first,update(t) };
 		}
 		else {
-			pair<Node*, Node*> s = split(move(t->rch), k - size(t->lch) - 1);
-			t->rch = move(s.first);
-			return { update(move(t)),move(s.second) };
+			pair<Node*, Node*> s = split(t->rch, k - size(t->lch) - 1);
+			t->rch = s.first;
+			return { update(t),s.second };
 		}
 	}
 
 	Node* insert(Node* t, int k, S v) {
-		auto [l, r] = split(move(t), k);
-		Node* ret = merge(move(l), new Node(v, rnd.random()));
-		ret = merge(move(ret), move(r));
+		auto [l, r] = split(t, k);
+		Node* ret = merge(l, new Node(v, rnd(mt)));
+		ret = merge(ret, r);
 		return ret;
 	}
 
 	Node* erase(Node* t, int k) {
-		auto [l, r_aux] = split(move(t), k);
-		auto [m, r] = split(move(r_aux), 1);
+		auto [l, r_aux] = split(t, k);
+		auto [m, r] = split(r_aux, 1);
 		delete m;
-		return merge(move(l), move(r));
+		return merge(l, r);
 	}
 
 	void reverse(Node* t, int l, int r) {
-		auto [b_aux, c] = split(move(t), r);
-		auto [a, b] = split(move(b_aux), l);
+		auto [b_aux, c] = split(t, r);
+		auto [a, b] = split(b_aux, l);
 		if (b != nullptr)b->rev ^= true;
-		auto t_aux = merge(move(a), move(b));
-		t = merge(move(t_aux), move(c));
+		auto t_aux = merge(a, b);
+		t = merge(t_aux, c);
 	}
 
 	void rotate(Node* t, int l, int m, int r) {
@@ -116,6 +103,7 @@ class Treap {
 	}
 
 public:
+	Treap() :mt(0), rnd(0) {}
 
 	int size() {
 		return size(root);
@@ -123,22 +111,22 @@ public:
 
 	void insert(int k, S v) {
 		assert(0 <= k and k <= size(root));
-		root = insert(move(root), k, v);
+		root = insert(root, k, v);
 	}
 
 	void erase(int k) {
 		assert(0 <= k and k < size(root));
-		root = erase(move(root), k);
+		root = erase(root, k);
 	}
 
 	S prod(int l, int r) {
 		assert(0 <= l and l <= r and r <= size(root));
-		auto [a, b] = split(move(root), r);
-		auto [c, d] = split(move(a), l);
+		auto [a, b] = split(root, r);
+		auto [c, d] = split(a, l);
 
 		S ret = prod(d);
 
-		root = merge(merge(move(c), move(d)), move(b));
+		root = merge(merge(c, d), b);
 
 		return ret;
 	}
